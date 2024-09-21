a07e9043678bebc291f952f8e9901d2bcb55f876
seonhyeokjun <tjsrmatn@naver.com>
2024-09-22 03:56:44 +0900

    메트릭 등록1 - 카운터

---
+++ src/main/java/hello/order/v1/OrderConfigV1.java
@@ -0,0 +1,14 @@
+package hello.order.v1;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV1 {
+    @Bean
+    OrderService orderService(MeterRegistry meterRegistry) {
+        return new OrderServiceV1(meterRegistry);
+    }
+}
---
+++ src/main/java/hello/order/v1/OrderServiceV1.java
@@ -0,0 +1,47 @@
+package hello.order.v1;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class OrderServiceV1 implements OrderService {
+    private final MeterRegistry meterRegistry;
+    private AtomicInteger stock = new AtomicInteger(100);
+
+    public OrderServiceV1(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    @Override
+    public void order() {
+        log.info("주문");
+        stock.decrementAndGet();
+
+        Counter.builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tag("method", "order")
+                .description("order")
+                .register(meterRegistry).increment();
+    }
+
+    @Override
+    public void cancel() {
+        log.info("취소");
+        stock.incrementAndGet();
+
+        Counter.builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tag("method", "cancel")
+                .description("order")
+                .register(meterRegistry).increment();
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+}
